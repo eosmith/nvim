fcaf88f7d2368b7581175155c7c4bbe4ccc86d9b
Chad Hilton <chilton@andrew.cmu.edu>
2022-02-19 21:25:59 -0500

    Added back init.vim

--- lua/init.lua
+++ lua/init.lua
@@ -1,6 +1,20 @@
-require "chad.options"
-require "chad.icons"
-require "chad.plugins"
-require "chad.nvim-tree"
-require "chad.lualine"
-require "chad.keymaps"
+require("chad.options")
+require("chad.icons")
+require("chad.plugins")
+require("chad.nvim-tree")
+require("chad.lualine")
+require("chad.keymaps")
+
+P = function(v)
+	print(vim.inspect(v))
+	return v
+end
+
+if pcall(require, "plenary") then
+	RELOAD = require("plenary.reload").reload_module
+
+	R = function(name)
+		RELOAD(name)
+		return require(name)
+	end
+end
